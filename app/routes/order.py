6a1a3ad316a3e89a144ec1b1717299ac259e1826
kushalraga <kushalraga20@gmail.com>
2025-06-30 10:25:03 +0530

    get single order route added

--- app/routes/order.py
+++ app/routes/order.py
@@ -5,6 +5,8 @@ from app.schemas.order import OrderOut, OrderItemOut
 from app.auth.auth_handler import get_current_user
 from app.models.user import User 
 from app.models.order import Order
+from app.models.product import Product
+from app.models.orderItem import OrderItem
 
 router = APIRouter(prefix="/orders", tags=["orders"])
 
@@ -22,3 +24,20 @@ async def get_my_orders(
             detail="Looks empty here"
         )
     return my_orders
+
+@router.get("/{order_id}", response_model=OrderOut)
+async def get_single_order(
+    order_id: int,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    my_order = db.query(Order).filter(
+        Order.user_id == current_user.id,
+        Order.id == order_id
+    ).first()  
+    if not my_order:
+        raise HTTPException(
+            status_code=404,
+            detail="Order not found"
+        )
+    return my_order 
