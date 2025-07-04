66d125ecab842169dc11e62329b96a80e5ef395a
kushalraga <kushalraga20@gmail.com>
2025-07-04 11:59:21 +0530

    tsx removed

--- frontend/src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex-1">
-      <section className="container mx-auto py-8">
-        <h1 className="text-3xl font-bold">Welcome to Our Store</h1>
-      </section>
-    </main>
-  );
-}
